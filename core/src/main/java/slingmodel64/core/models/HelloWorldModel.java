8a59b76c757ce14c8bcce5087a869f5ea47e0caf
Kiran S G <kiransg89@gmail.com>
2019-10-23 13:22:11 +0530

    Committing code base to Master

---
+++ core/src/main/java/slingmodel64/core/models/HelloWorldModel.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright 2015 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package slingmodel64.core.models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.settings.SlingSettingsService;
+
+import com.day.cq.search.PredicateGroup;
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.search.facets.Bucket;
+import com.day.cq.search.facets.Facet;
+import com.day.cq.search.result.SearchResult;
+
+@Model(adaptables=Resource.class)
+public class HelloWorldModel {
+
+    @Inject
+    private SlingSettingsService settings;
+
+    @Inject @Named("sling:resourceType") @Default(values="No resourceType")
+    protected String resourceType;
+    
+	@Inject
+	private QueryBuilder queryBuilder;
+
+	@SlingObject
+	private ResourceResolver resourceResolver;
+
+    private String message;
+
+    @PostConstruct
+    protected void init() {
+    	final Map<String, String> map = new HashMap<String, String>();
+
+		// https://docs.adobe.com/content/docs/en/aem/6-2/develop/ref/javadoc/com/day/cq/search/eval/PathPredicateEvaluator.html
+		map.put("path", "/content/we-retail/language-masters/en/products");
+		map.put("1_property", "cq:tags");
+		map.put("fulltext", "pants");
+		map.put("p.facets", "true");
+
+		//ResourceResolver resourceResolver = request.getResourceResolver();
+		Query query = queryBuilder.createQuery(PredicateGroup.create(map), resourceResolver.adaptTo(Session.class));
+
+		SearchResult result = query.getResult();
+		ArrayList<String> Keyss = new ArrayList<String>();
+
+		Map<String, Facet> facets;
+		try {
+			facets = result.getFacets();
+			
+			for (String key : facets.keySet()) {
+				Facet facet = facets.get(key);
+				if (facet.getContainsHit()) {
+					for (Bucket bucket : facet.getBuckets()) {
+						bucket.getCount();
+						Keyss.add(bucket.getValue());
+					}
+				}
+			}
+		} catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        message = "\tHello World!\n";
+        message += "\tThis is instance: " + settings.getSlingId() + "\n";
+        message += "\tResource type is: " + resourceType + "\n";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
